8f80dd16b3adbf15c8030cc67773b0f10cf9ace3
Angel-Borja <angelborja@ciencias.unam.mx>
2023-10-02 08:22:19 -0600

    Versión 1

--- app/src/main/java/com/example/tarea2/Inicio.kt
+++ app/src/main/java/com/example/tarea2/Inicio.kt
@@ -1,8 +1,6 @@
 package com.example.tarea2
 
-import android.content.*
 import android.os.Bundle
-import android.widget.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,18 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tarea2.ui.theme.Tarea2Theme
 
-class MainActivity : ComponentActivity() {
+class Inicio : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
-
-        val inicioActBtn = findViewById<Button>(R.id.ingresarBtn)
-        inicioActBtn.setOnClickListener {
-            val Intent = Intent(this, Inicio::class.java)
-            startActivity(Intent)
+        setContent {
+            setContentView(R.layout.inicio);
         }
     }
-        private fun irInicio() {
+}
 
-        }
-        }
